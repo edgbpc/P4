98f91253818f4ec4b20823e1dfe52ad66382be9b
ericgoodwin <edgbpc@mail.umsl.edu>
2019-05-11 21:56:54 -0500

    .

--- P4/parser.cpp
+++ P4/parser.cpp
@@ -364,8 +364,8 @@ node* Parser::assign() {
     node->child2 = NULL;
     node->child3 = NULL;
     node->child4 = NULL;
-    tkScanner();
     if (receivedToken.tokenID == identifierToken){
+        tkScanner();
         if (receivedToken.tokenInstance == "=") {
 
             node->child1 = expr();
